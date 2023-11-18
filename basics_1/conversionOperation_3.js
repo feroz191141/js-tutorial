309b83291206a4519ea6991a638e6c5790b262d0
Feroz <feroz191141@gmail.com>
2023-11-18 21:30:54 +0530

    conversionof datatypes

---
+++ basics_1/conversionOperation_3.js
@@ -0,0 +1,22 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//boolean conversion
+
+let isLoggedIn= 0
+let booleanLogged = Boolean(isLoggedIn)
+
+console.log(booleanLogged);
+
+//sring conversion
+
+let someNumber = 33 
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
