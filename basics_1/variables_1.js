e9c21937af2e76323c3b0ba5eaa148005cf077b5
feroz191141 <83948821+feroz191141@users.noreply.github.com>
2023-11-18 13:53:38 +0000

    video 4 changes

---
+++ basics_1/variables_1.js
@@ -0,0 +1,18 @@
+
+const account_id = 12341
+let email="feroz@gmail.com"
+var account_pass="asda"
+let avaraible
+
+// account_id=12312 not allowed overiding
+
+console.log(account_id);
+
+/*
+donot use var as it has the funvtional block error and scope error
+*/
+
+email="fs@gmail,.com"
+account_pass='asdasdaf'
+
+console.table([email , account_pass , avaraible])
