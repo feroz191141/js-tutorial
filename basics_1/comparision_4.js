c6fa2e0ba89340b22623b1430fea0e03f79a93d1
Feroz <feroz191141@gmail.com>
2023-11-19 01:03:52 +0530

    datatypes and operations

---
+++ basics_1/comparision_4.js
@@ -0,0 +1,17 @@
+
+// console.log( 2 > 1);
+// console.log( 2 == 1);
+// console.log( 2 < 1);
+// console.log( 2 >= 1);
+// console.log( 2 <= 1);
+// console.log( 2 != 1);
+
+// console.log(null == 0); false
+// console.log(null > 0);  false
+// console.log(null >= 0); here it is false becuase >= operator works some differently so changed it to zero
+
+console.log("2" == 2) // here it is true because changed string to number
+
+// === strict operator **************
+
+console.log("2" === 2)  //here it is false
