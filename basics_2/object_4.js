86561285f89719f9092a3a6e1b85bf673551d731
Feroz <feroz191141@gmail.com>
2023-11-20 14:39:13 +0530

    object part three learnt

---
+++ basics_2/object_4.js
@@ -0,0 +1,48 @@
+// const tinderUser  = new Object()
+
+const  tinderUser = {}
+
+tinderUser.id = "123abc"
+tinderUser.name = "feroz"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email : "feroz@gmail.com",
+    userFullName:{
+        firstName: "Feroz",
+        lastName: "Mohammad"
+    }
+}
+
+// console.log(regularUser.email);
+// console.log(regularUser.userFullName.firstName);
+// console.log(regularUser.userFullName.lastName);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+// const obj3= {obj1 , obj2}
+// console.log(obj3);
+
+// const obj3 = Object.assign({},obj1,obj2)
+
+// console.log(obj3);
+
+const obj3 = {...obj1,...obj2}
+// console.log(obj3);
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+//*****************************object part 3**************
+
+
+
+const {isLoggedIn} = tinderUser
+console.log(isLoggedIn);
