62a1d1c32602c6062253b34e86e473a8b32a26a0
Feroz <feroz191141@gmail.com>
2023-11-20 22:35:17 +0530

    switch case

---
+++ controlFlow_4/switch.js
@@ -0,0 +1,31 @@
+
+
+// switch (key) {
+//     case value:
+        
+//         break;           // syntax ofswitch
+
+//     default:
+//         break;
+// }
+
+const month = 7
+
+switch (month) {
+    case 1:
+        console.log(`january`);
+        break;
+    case 2:
+        console.log(`feb`);
+        break;
+    case 3:
+        console.log(`march`);
+        break;
+    case 4:
+        console.log(`april`);
+        break;
+    default:
+        console.log(`default case`);
+        break;
+}
+
